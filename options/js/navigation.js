4e8532e832ec5db129adc8b99ea76cf9ce58132c
Moros1138 <moros1138@gmail.com>
2016-11-02 11:07:16 -0400

    turn every navbar link into a page

---
+++ options/js/navigation.js
@@ -0,0 +1,49 @@
+
+var MANavigation = (function() {
+	
+	var pages = [
+		'#home',
+		'#chat',
+		'#options',
+		'#savedMsg',
+		'#tipwars',
+		'#tokenkeno'
+	];
+	
+	function navigateTo(page) {
+		
+		if( pages.indexOf(page) != -1 ) {
+			for(var i = 0; i < pages.length; i++) {
+				$(pages[i]).hide();
+			}
+			$(page).show();
+		} else {
+			navigateTo('home');
+		}
+		
+	}
+	
+	$(document).ready(function() {
+		
+		// page load navigation
+		if(window.location.hash) {
+			navigateTo(window.location.hash);
+		} else {
+			navigateTo('#home');
+		}
+		
+		
+		// link click navigation
+		$('a').click(function(e) {
+			navigateTo($(this).attr('href'));
+		});
+		
+		
+		
+	});	
+	
+	return {
+	};
+	
+})();
+
