5a494241384e73fed3646b23174e8af6dd806156
Moros1138 <moros1138@gmail.com>
2016-11-05 10:15:49 -0400

    removed onConnect listener
    it was causing an error when the options page wasn't open and it doesn't
    seem to do anything.. pertinent anyways.

--- background/background.js
+++ background/background.js
@@ -52,27 +52,6 @@ chrome.runtime.onMessage.addListener(function (request, sender, response) {
 	
 });
 
-chrome.extension.onConnect.addListener(function(port) {
-
-	var tab = port.sender.tab;
-
-	/**
-	 * This will get called by the content script we execute in the
-	 * tab as a result of the user pressing the browser action.
-	 ******************************************************************/
-	port.onMessage.addListener(function(info) {
-
-		var max_length = 1024;
-		
-		if (info.selection.length > max_length)
-			info.selection = info.selection.substring(0, max_length);
-
-		openOrFocusOptionsPage();
-		
-	});
-	
-});
-
 /**
  * Open options page when browserAction is clicked
  ******************************************************************/
