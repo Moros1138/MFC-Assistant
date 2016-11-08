71af94fbd7afa59c79bef477af634ca84882ad71
Moros1138 <moros1138@gmail.com>
2016-11-08 16:52:40 -0500

    Revert "update gitignore"
    This reverts commit 48c48e12c30ef66b5ded8aa372f441936cead499.

--- build-tools/build.php
+++ build-tools/build.php
@@ -2,24 +2,15 @@
 
 require('config.php');
 
-// make sure DISTPATH exists
-if( !file_exists(DISTPATH)) mkdir(DISTPATH);
-
 /**
  * CHROME EXTENSION
  ******************************************/
-if( !file_exists(DISTPATH.'/chrome')) mkdir(DISTPATH.'/chrome');
 rcopy( SRCPATH, DISTPATH.'/chrome');
 update_home_page('chrome');
-
-$path = str_replace( '/', '\\', DISTPATH.'/chrome' );
-chdir($path);
-system( 'zip -r "..\MFC Assistant (Chrome).zip" .' );
 
 /**
  * FIREFOX EXTENSION
  ******************************************/
-if( !file_exists(DISTPATH.'/firefox')) mkdir(DISTPATH.'/firefox');
 rcopy( SRCPATH, DISTPATH.'/firefox');
 update_home_page('firefox');
 
