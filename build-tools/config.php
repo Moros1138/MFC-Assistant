1ee3357a81cffa44b9569b5c05aace8f82e465cc
Moros1138 <moros1138@gmail.com>
2016-11-08 16:33:46 -0500

    add firefox api

--- build-tools/config.php
+++ build-tools/config.php
@@ -4,7 +4,9 @@ define(	'ABSPATH', dirname(dirname(__FILE__)) );
 define( 'SRCPATH', ABSPATH . '/src' );
 define( 'DISTPATH', ABSPATH . '/dist' );
 
+$jwt_issuer = ''; // Set your mozilla issuer
+$jwt_secret = ''; // Set your mozilla secret
+
 require( ABSPATH . '/build-tools/libs/parsedown/Parsedown.php' );
 require( ABSPATH . '/build-tools/inc/functions.php' );
-
-$pd = new Parsedown();
+require( ABSPATH . '/build-tools/inc/mozilla-api.php' );
