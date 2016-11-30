1691a450c5559ee1e5924252c0a1f1ffee5c585e
Moros1138 <moros1138@gmail.com>
2016-11-29 20:00:37 -0500

    Remove unused packages.

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,10 @@
-const fs = require("fs")
-const gulp = require("gulp")
-const bump = require("gulp-bump")
-const clean = require("gulp-clean")
-const replace = require("gulp-replace")
-const jeditor = require("gulp-json-editor")
-const zip = require('gulp-zip')
-const runSequence = require('run-sequence')
+const gulp			= require("gulp")
+const bump			= require("gulp-bump")
+const clean			= require("gulp-clean")
+const replace		= require("gulp-replace")
+const jeditor		= require("gulp-json-editor")
+const zip			= require('gulp-zip')
+const runSequence	= require('run-sequence')
 
 /**
  * This configuration contains exported
@@ -74,6 +73,20 @@ gulp.task('clean', () => {
 	
 });
 
+gulp.task('bump-major', () => {
+	
+	gulp.src('package.json').pipe(bump({type:'major'})).pipe(gulp.dest(''));
+	return gulp.src('src/manifest.json').pipe(bump({type:'major'})).pipe(gulp.dest('src'));
+	
+});
+
+gulp.task('bump-minor', () => {
+	
+	gulp.src('package.json').pipe(bump({type:'minor'})).pipe(gulp.dest(''));
+	return gulp.src('src/manifest.json').pipe(bump({type:'minor'})).pipe(gulp.dest('src'));
+	
+});
+
 gulp.task('bump', () => {
 	
 	gulp.src('package.json').pipe(bump()).pipe(gulp.dest(''));
@@ -89,83 +102,86 @@ gulp.task('build', () => {
 		gulp.src([
 			'src/**',
 			'!src/manifest.json',
+			'!src/firefox-build.cmd',
 			'!src/options/js/components/**',
 			'!src/options/js/components'
 		])
 		.pipe(gulp.dest('dist/chrome'))
 		.pipe(gulp.dest('dist/firefox'))
-		
-		// chrome manifest
-		gulp.src([
-			'src/manifest.json'
-		])
-		.pipe(gulp.dest("dist/chrome"))
 		.on('finish', () => {
 			
-			setTimeout(() => {
-				var date = new Date();
-				var version = date.getFullYear()+'-'+(date.getMonth()+1)+'-'+date.getDate()+' '+date.getHours()+'.'+date.getMinutes()+'.'+date.getSeconds()
-				
-				gulp.src([
-					"dist/chrome/**"
-				])
-				.pipe(zip('mfc-assistant-chrome.'+version+'.zip'))
-				.pipe(gulp.dest('dist'))
-				.on('finish', () => {
-
-					setTimeout(() => {
-
-						if(config.betaDeploy != undefined) {
-						
-							gulp.src([
-								"dist/mfc-assistant-chrome.*.zip"
-							])
-							.pipe(gulp.dest(config.betaDeploy))
-							
-						}
-						
-						console.log('Finished Chrome.')
-						
-					}, 1000)
+			// chrome manifest
+			gulp.src([
+				'src/manifest.json'
+			])
+			.pipe(gulp.dest("dist/chrome"))
+			.on('finish', () => {
+		
+				setTimeout(() => {
+					var date = new Date();
+					var version = date.getFullYear()+'-'+(date.getMonth()+1)+'-'+date.getDate()+' '+date.getHours()+'.'+date.getMinutes()+'.'+date.getSeconds()
 					
-				})
-			}, 1000);
-			
-		});
-		
+					gulp.src([
+						"dist/chrome/**"
+					])
+					.pipe(zip('mfc-assistant-chrome.'+version+'.zip'))
+					.pipe(gulp.dest('dist'))
+					.on('finish', () => {
 
-		// firefox manifest
-		return gulp.src([
-			'src/manifest.json'
-		])
-		.pipe(jeditor(function(json){
+						setTimeout(() => {
+
+							if(config.betaDeploy != undefined) {
+							
+								gulp.src([
+									"dist/mfc-assistant-chrome.*.zip"
+								])
+								.pipe(gulp.dest(config.betaDeploy))
+								
+							}
+							
+							console.log('Finished Chrome.')
+							
+						}, 1000)
+						
+					})
+				}, 1000);
+				
+			});
 			
-			if(config.appID && config.updateURL) {
-				json.applications = {
-					gecko: {
-						id: config.appID,
-						strict_min_version: '42.0',
-						strict_max_version: '50.*',
-						update_url: config.updateURL
-					}
-				};
-			}
-			
-			return json;
-		}))
-		.pipe(gulp.dest("dist/firefox"))
-		.on('finish', () => {
-			setTimeout(() => {
-				if(config.issuer && config.secret) {
-					gulp.src('src/firefox-build.cmd')
-						.pipe(replace('JWT_ISSUER', config.issuer))
-						.pipe(replace('JWT_SECRET', config.secret))
-						.pipe(gulp.dest('dist/firefox'))
-						.on('finish', () => {
-							console.log('Finished Firefox.');
-						});
+			// firefox manifest
+			gulp.src([
+				'src/manifest.json'
+			])
+			.pipe(jeditor(function(json){
+				
+				if(config.appID && config.updateURL) {
+					json.applications = {
+						gecko: {
+							id: config.appID,
+							strict_min_version: '42.0',
+							strict_max_version: '50.*',
+							update_url: config.updateURL
+						}
+					};
 				}
-			}, 1000);
+				
+				return json;
+			}))
+			.pipe(gulp.dest("dist/firefox"))
+			.on('finish', () => {
+				setTimeout(() => {
+					if(config.issuer && config.secret) {
+						gulp.src('src/firefox-build.cmd')
+							.pipe(replace('JWT_ISSUER', config.issuer))
+							.pipe(replace('JWT_SECRET', config.secret))
+							.pipe(gulp.dest('dist/firefox'))
+							.on('finish', () => {
+								console.log('Finished Firefox.');
+							});
+					}
+				}, 1000);
+			})
+			
 		})
 		
 	});
