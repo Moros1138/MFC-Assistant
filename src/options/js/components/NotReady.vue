bc2184822e43304dd2fe9be20a2abb876ac00838
Moros1138 <moros1138@gmail.com>
2016-11-29 21:46:53 -0500

    Explain Not Ready in more detail

--- src/options/js/components/NotReady.vue
+++ src/options/js/components/NotReady.vue
@@ -2,11 +2,23 @@
 	<div id="not-ready">
 		<div class="panel panel-default">
 			<div class="panel-heading">
-				<h3 class="panel-title">Waiting for Model Web</h3>
+				<h3 class="panel-title">Waiting for Chat Room</h3>
 			</div>
 			<div class="panel-body">
-				The MFC Assistant is currently waiting for the
-				Broadcaster to be started.
+				<p>
+					The MFC Assistant is currently waiting for the chat room to be accessible.
+				</p>
+				<ul>
+					<li>
+						<b>On the Model Web:</b> the chat room is accessible when you begin broadcasting.
+					</li>
+					<li>
+						<b>On the Member's site:</b> the chat room is accessible when you enter your own
+						chat room. The MFC Assistant will only work for models logged into their
+						model account and are in their own rooms. This prevents other users from
+						using the MFC Assistant in a model's room without their consent!
+					</li>
+				</ul>
 			</div>
 		</div>
 	</div>
